cbc6629b6596d58aafff98cc38cad8345b6d636f
jeka2993 <jeka20001980@gmail.com>
2020-08-01 11:47:58 +0300

    add card block in balance

--- src/pages/portfolio/components/BalanceCard.jsx
+++ src/pages/portfolio/components/BalanceCard.jsx
@@ -17,6 +17,36 @@ export default class BalanceCard extends React.Component {
                         <i className="las la-ellipsis-h"></i>
                     </div>
                 </div>
+                <div className="card-item">
+                    <div className="card-category">
+                        <div className="card-icon">
+                            <i class="lab la-cc-visa"></i>
+                        </div>
+                        <div className="card-number">
+                            <p>3210 **** **** 4008</p>
+                            <span>Номер карты</span>
+                        </div>
+                    </div>
+                    <div className="card-info-text">
+                        <p>$88.200 <i class="las la-arrow-up"></i></p>
+                        <span>Баланс</span>
+                    </div>
+                </div>
+                <div className="card-item">
+                    <div className="card-category">
+                        <div className="card-icon">
+                            <i class="lab la-cc-mastercard"></i>
+                        </div>
+                        <div className="card-number">
+                            <p>4008 **** **** 9464</p>
+                            <span>Номер карты</span>
+                        </div>
+                    </div>
+                    <div className="card-info-text">
+                        <p>$66.400 <i class="las la-arrow-down"></i></p>
+                        <span>Баланс</span>
+                    </div>
+                </div>
             </div>
         );
     }
