19dd787df4c6c9003a2759f5a0a19a71857529aa
jeka2993 <jeka20001980@gmail.com>
2020-08-06 13:41:30 +0300

    low fix

--- src/pages/portfolio/components/Profile.jsx
+++ src/pages/portfolio/components/Profile.jsx
@@ -33,109 +33,113 @@ class Profile extends React.Component {
         const loading = this.props.loading;
 
         return (
+        <div className="profile-wrapper">
             <div className="profile">
-                {!loading && user &&
-                    <div className="profile-block profile-loading">
-                        <div className="profile-wrapper">
-                            <h5>Главная информация</h5>
-                            <div className="profile-avatar-block">
-                                <div className="profile-avatar-box">
-                                    <img className="profile-avatar" src={avatar2} alt="avatar" />
-                                    <h4>{user.firstName} {user.lastName}</h4>
+                <div className="profile-container">
+                    {!loading && user &&
+                        <div className="profile-block profile-loading">
+                            
+                            
+                                <h5>Главная информация</h5>
+                                <div className="profile-avatar-block">
+                                    <div className="profile-avatar-box">
+                                        <img className="profile-avatar" src={avatar2} alt="avatar" />
+                                        <h4>{user.firstName} {user.lastName}</h4>
+                                    </div>
+                                </div>
+                                <div className="profile-settings">
+                                    <Form>
+                                        <Row>
+                                            <Col>
+                                                <Form.Group controlId="formBasicEmail">
+                                                    <Form.Label>Имя</Form.Label>
+                                                    <Form.Control value={this.state.firstName} onChange={this.onInputChange} placeholder="Ваше имя" />
+                                                </Form.Group>
+                                            </Col>
+                                            <Col>
+                                                <Form.Group controlId="formBasicEmail">
+                                                    <Form.Label>Фамилия</Form.Label>
+                                                    <Form.Control value={this.state.lastName} onChange={this.onInputChange} placeholder="Ваша фамилия" />
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <Form.Group controlId="formBasicEmail">
+                                                    <Form.Label>Email</Form.Label>
+                                                    <Form.Control value={this.state.email} onChange={this.onInputChange} placeholder="Ваш Email" />
+                                                </Form.Group>
+                                            </Col>
+                                            <Col>
+                                                <Form.Group>
+                                                    <Form.Label>Телефон</Form.Label>
+                                                    <Form.Control
+                                                        type="number"
+                                                        placeholder="Ваш номер телефона"
+                                                        disabled
+                                                    />
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <Form.Label>Язык</Form.Label>
+                                                <Form.Control disabled as="select">
+                                                    <option>Русский</option>
+                                                    <option>English</option>
+                                                    <option>Українська</option>
+                                                    <option>Deutschland</option>
+                                                </Form.Control>
+                                            </Col>
+                                            <Col>
+                                                <Form.Label>Валюта</Form.Label>
+                                                <Form.Control disabled as="select">
+                                                    <option>RUS</option>
+                                                    <option>USD</option>
+                                                    <option>EUR</option>
+                                                    <option>GBP</option>
+                                                    <option>UAH</option>
+                                                </Form.Control>
+                                            </Col>
+                                        </Row>
+                                    </Form>
+                                    <Form>
+                                        <Button variant="primary" type="submit" className="orange-btn">
+                                            ОБНОВИТЬ НАСТРОЙКИ
+                                                    </Button>
+                                        <Button variant="primary" type="submit" className="back">
+                                            НАЗАД
+                                                    </Button>
+                                    </Form>
                                 </div>
                             </div>
-                            <div className="profile-settings">
-                                <Form>
-                                    <Row>
-                                        <Col>
-                                            <Form.Group controlId="formBasicEmail">
-                                                <Form.Label>Имя</Form.Label>
-                                                <Form.Control value={this.state.firstName} onChange={this.onInputChange} placeholder="Ваше имя" />
-                                            </Form.Group>
-                                        </Col>
-                                        <Col>
-                                            <Form.Group controlId="formBasicEmail">
-                                                <Form.Label>Фамилия</Form.Label>
-                                                <Form.Control value={this.state.lastName} onChange={this.onInputChange} placeholder="Ваша фамилия" />
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <Form.Group controlId="formBasicEmail">
-                                                <Form.Label>Email</Form.Label>
-                                                <Form.Control value={this.state.email} onChange={this.onInputChange} placeholder="Ваш Email" />
-                                            </Form.Group>
-                                        </Col>
-                                        <Col>
-                                            <Form.Group>
-                                                <Form.Label>Телефон</Form.Label>
-                                                <Form.Control
-                                                    type="number"
-                                                    placeholder="Ваш номер телефона"
-                                                    disabled
-                                                />
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <Form.Label>Язык</Form.Label>
-                                            <Form.Control disabled as="select">
-                                                <option>Русский</option>
-                                                <option>English</option>
-                                                <option>Українська</option>
-                                                <option>Deutschland</option>
-                                            </Form.Control>
-                                        </Col>
-                                        <Col>
-                                            <Form.Label>Валюта</Form.Label>
-                                            <Form.Control disabled as="select">
-                                                <option>RUS</option>
-                                                <option>USD</option>
-                                                <option>EUR</option>
-                                                <option>GBP</option>
-                                                <option>UAH</option>
-                                            </Form.Control>
-                                        </Col>
-                                    </Row>
-                                </Form>
-                                <Form>
-                                    <Button variant="primary" type="submit" className="orange-btn">
-                                        ОБНОВИТЬ НАСТРОЙКИ
-                                                </Button>
-                                    <Button variant="primary" type="submit" className="back">
-                                        НАЗАД
-                                                </Button>
-                                </Form>
-                            </div>
+                    }
+                    <div className="profile-sidebar">
+                        <div className="profile-sidebar-item">
+                            <p><a href="#">Главная информация</a></p>
+                            <p><a href="#">Безопасность</a></p>
+                            <p><a href="#">Уведомления</a></p>
                         </div>
-                    </div>
-                }
-                <div className="profile-sidebar">
-                    <div className="profile-sidebar-item">
-                        <p><a href="#">Главная информация</a></p>
-                        <p><a href="#">Безопасность</a></p>
-                        <p><a href="#">Уведомления</a></p>
-                    </div>
-                    <div className="profile-sidebar-help">
-                        <p><a href="#">Центр помощи</a></p>
-                        <p><a href="#">Связаться с нами</a></p>
-                        <p><a href="#">Закрыть аккаунт</a></p>
+                        <div className="profile-sidebar-help">
+                            <p><a href="#">Центр помощи</a></p>
+                            <p><a href="#">Связаться с нами</a></p>
+                            <p><a href="#">Закрыть аккаунт</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
         );
     }
-}
+
 
 function mapStateToProps(state) {
-    const { data, loading } = state.profile;
+    const { data, loading} = state.profile;
 
     return {
-        data,
-        loading
-    };
+          data,
+          loading
+        };
 }
 
 const connectedProfile = connect(mapStateToProps)(Profile);
